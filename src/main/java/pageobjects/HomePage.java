631ccbbdc2bbca380672d5e38381ece7b4b0cf5a
vikas-jangra-exa <97954930+vikas-jangra-exa@users.noreply.github.com>
2022-02-06 13:44:15 +0530

    Automated login page TCs

---
+++ src/main/java/pageobjects/HomePage.java
@@ -0,0 +1,26 @@
+package pageobjects;
+
+import actiondriver.Action;
+import actiondriver.ActionInterface;
+import base.BaseClass;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePage extends BaseClass
+{
+    ActionInterface action=new Action();
+    @FindBy(xpath = "//a[contains(text(),'Welcome')]")
+    WebElement WelcomeText;
+
+
+    public HomePage()
+    {
+        PageFactory.initElements(driver,this);
+    }
+
+    public String getWelcomeText()
+    {
+        return action.getText(WelcomeText);
+    }
+}
---
+++ src/test/java/com/mystore/testcases/LoginPageTest.java
@@ -0,0 +1,50 @@
+package com.mystore.testcases;
+
+import actiondriver.Action;
+import actiondriver.ActionInterface;
+import base.BaseClass;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pageobjects.HomePage;
+import pageobjects.IndexPage;
+import pageobjects.LoginPage;
+import pageobjects.ResponsePopupBox;
+
+
+public class LoginPageTest extends BaseClass
+{
+    ActionInterface action=new Action();
+    IndexPage indexPage;
+    LoginPage loginPage;
+    HomePage homePage;
+    @BeforeMethod
+    public void setup()
+    {
+        action.openBrowser();
+        indexPage=new IndexPage();
+    }
+    @AfterMethod
+    public void tearDown()
+    {
+        action.closeBrowser();
+    }
+
+    @Test(groups = "sanity")
+    public void LoginWithCorrectCredentials()
+    {
+       loginPage=indexPage.clickOnLoginBtn();
+       Assert.assertEquals(loginPage.pageTitle(),"Log in");
+       homePage=loginPage.login(prop.getProperty("username"),prop.getProperty("password"));
+       Assert.assertEquals(homePage.getWelcomeText(),"Welcome "+prop.getProperty("username"));
+    }
+    @Test
+    public void LoginWithIncorrectCredentials()
+    {
+        loginPage=indexPage.clickOnLoginBtn();
+        Assert.assertEquals(loginPage.pageTitle(),"Log in");
+        ResponsePopupBox response =loginPage.loginWithInvalidCredentials(prop.getProperty("username"),"" );
+        Assert.assertEquals(response.getResponseMessage(),"Please fill out Username and Password.");
+    }
+}
